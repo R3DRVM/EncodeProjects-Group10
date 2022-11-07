3645cd0f94f6f774e7cca43eea3e61c83bc1a0f7
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-11-07 08:08:39 -0800

    Update vote.ts

--- vote.ts
+++ vote.ts
@@ -4,10 +4,11 @@ import * as dotenv from 'dotenv';
 dotenv.config()
 
 async function main() {
-const contractAddress = "0xF2299632a664E2d445ee11E42Af3B9AA7eFA2c84";
+  //input contract address and proposal index to vote
+const contractAddress = process.argv[2];
 const proposalIndex = process.argv[3];
 const provider = ethers.getDefaultProvider("goerli", {alchemy: process.env.ALCHEMY_API_KEY});
-const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "");
+const wallet = new ethers.Wallet(process.env.MNEMONIC ?? "");
 const signer = wallet.connect(provider);
 const ballotContractFactory = new Ballot__factory(signer);
     const ballotContract = ballotContractFactory.attach(
