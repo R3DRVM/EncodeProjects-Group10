fd6d37e2cad1e8804d737b41b6f9a119ba7ed7ae
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-11-07 08:15:53 -0800

    Update GiveRightToVote.ts

--- GiveRightToVote.ts
+++ GiveRightToVote.ts
@@ -1,28 +1,25 @@
-import { ethers } from "ethers";
+import { ethers } from 'ethers';
 import { Ballot__factory } from "../typechain-types";
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv' 
 dotenv.config()
 
 async function main() {
+
   const contractAddress = process.argv[2];
   const targetAddress = process.argv[3];
-  const provider = ethers.getDefaultProvider("goerli", {alchemy: process.env.ALCHEMY_API_KEY});
-  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "");
+  const provider = ethers.getDefaultProvider("goerli", {etherscan: process.env.ETHERSCAN_API_KEY});
+  const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? ""); // get wallet from metamask
   const signer = wallet.connect(provider);
-  console.log(`Connected to the wallet ${signer.address}`);
-  const balance = await signer.getBalance();
-  console.log(`This address has a balance of ${balance} wei`);
-  if (balance.eq(0)) throw new Error("Insufficient funds");
-  const ballotContractFactory = new Ballot__factory(signer);
-    const ballotContract = ballotContractFactory.attach(
-       contractAddress
-    );
-    const tx = await ballotContract.vote(targetAddress);
-    await tx.wait();
-    console.log("Voting rights have been granted");
-    console.log(tx.hash);
+  console.log(`I am connected to ${signer.address}`)
+  // TODO
+  const ballotContractFactory =  new Ballot__factory(signer);
+  const ballotContract = await ballotContractFactory.attach(contractAddress);
+  const tx = await ballotContract.giveRightToVote(targetAddress)
+  await tx.wait()
+  console.log(`gave right to vote to ${targetAddress} for contract ${contractAddress}`)
+  console.log(`transaction hash is ${tx.hash}`)
 }
-  
+
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
