ea7b6ac901739fcd2b4324eb393ee1b91e3994a5
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-11-06 23:56:20 -0800

    Update Query.ts

--- Query.ts
+++ Query.ts
@@ -6,7 +6,7 @@ dotenv.config()
 async function main() {
     const contractAddress = "0xF2299632a664E2d445ee11E42Af3B9AA7eFA2c84"
     const provider = ethers.getDefaultProvider("goerli", {alchemy: process.env.ALCHEMY_API_KEY});
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+    const wallet = new ethers.Wallet(process.env.MNEMONIC ?? "");
     const signer = wallet.connect(provider);
     const ballotContractFactory = new Ballot__factory(signer);
     const ballotContract = ballotContractFactory.attach(
@@ -16,8 +16,6 @@ async function main() {
     const tx1 = await ballotContract.winningProposal();
     console.log("The winning proposal is:");
     console.log(ethers.utils.parseBytes32String(tx));
-    console.log("The winning proposal has the following # of votes:");
-    console.log(tx1);
 }
 
 main().catch((error) => {
