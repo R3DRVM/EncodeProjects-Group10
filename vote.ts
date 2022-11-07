2a6fd87527584aa2190c8219862aa14b22eb234f
R3DRVM <99007375+R3DRVM@users.noreply.github.com>
2022-11-07 00:28:29 -0800

    Update vote.ts

--- vote.ts
+++ vote.ts
@@ -7,7 +7,8 @@ async function main() {
 const contractAddress = "0xF2299632a664E2d445ee11E42Af3B9AA7eFA2c84";
 const proposalIndex = process.argv[3];
 const provider = ethers.getDefaultProvider("goerli", {alchemy: process.env.ALCHEMY_API_KEY});
-const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "");const signer = wallet.connect(provider);
+const wallet = ethers.Wallet.fromMnemonic(process.env.MNEMONIC ?? "");
+const signer = wallet.connect(provider);
 const ballotContractFactory = new Ballot__factory(signer);
     const ballotContract = ballotContractFactory.attach(
       contractAddress
